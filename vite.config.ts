08fb111b34f3c594029ee2d8bc9e28de2de5f14e
Abdallah <138681996+AbdallahMuhammad2@users.noreply.github.com>
2025-02-24 13:42:26 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: ({name}) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-            return 'assets/images/[name]-[hash][extname]';
-          }
-          if (/\.css$/.test(name ?? '')) {
-            return 'assets/css/[name]-[hash][extname]';
-          }
-          if (/\.(mp4|webm)$/.test(name ?? '')) {
-            return 'assets/videos/[name]-[hash][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/vertice',
 });
